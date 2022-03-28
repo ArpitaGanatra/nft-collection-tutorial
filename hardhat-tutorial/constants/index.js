a9dc8f373d04094f38a57771e6021f303ad02d31
Arpita Ganatra <arpitaganatra@Arpitas-MacBook-Air.local>
2022-03-28 19:55:52 +0530

    update metadata url

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,4 +1,4 @@
 const WHITLIST_CONTRACT_ADDRESS="0x04bBdDF298e04cb7bf19bAd3cE80c31dE8502bF5";
-const METADATA_URL="https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL="https://nft-collection-tutorial.vercel.app/api/";
 
 module.exports = {WHITLIST_CONTRACT_ADDRESS, METADATA_URL};
